b5a29355023fed9f2bb6203f6aec09eca1b67caf
Milena Lara <milenalrf@gmail.com>
2023-07-06 12:19:03 -0300

    updating README.md

---
+++ build/precache-manifest.3d0a3e9e2f3dcbe094be693106b0f0cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858c21151f6e7b6e0942be5f71a7411f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1888cf3b132eafb3c547",
+    "url": "/static/css/main.58ca51f5.chunk.css"
+  },
+  {
+    "revision": "32b8ef5759027aed5986",
+    "url": "/static/js/2.999105b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.999105b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1888cf3b132eafb3c547",
+    "url": "/static/js/main.7b4fc558.chunk.js"
+  },
+  {
+    "revision": "cb104cade9e4b303eb10",
+    "url": "/static/js/runtime-main.71ac8d74.js"
+  },
+  {
+    "revision": "0f2ed746d9022112be22e61fc68f4b65",
+    "url": "/static/media/email-icon.0f2ed746.png"
+  },
+  {
+    "revision": "4cfec347844ef3ba76e72b29674190e9",
+    "url": "/static/media/profilepic.4cfec347.jpg"
+  }
+]);
